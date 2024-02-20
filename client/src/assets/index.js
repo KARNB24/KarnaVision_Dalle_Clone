360971d39e5c67921fd702489245e77580e2923e
KARNB24 <bhavkarn@sheridancollege.ca>
2024-02-20 18:41:13 -0500

    Created required components and pages

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png';
-import logo from './logo.svg';
+
+
+import openai from './openai.png';
 import preview from './preview.png';
 
 export {
-  download,
-  logo,
-  preview,
+  download,  
+  preview,  
+  openai,
 };
