96718fe60bbb72c4a322031ff149e414a758cf2d
KARNB24 <bhavkarn@sheridancollege.ca>
2024-02-21 17:15:07 -0500

    Hosted backend on render.com

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,7 +21,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        const response = await fetch("https://karnavision-dalle-clone.onrender.com/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -50,7 +50,7 @@ const CreatePost = () => {
       setLoading(true);
       try {
         const response = await fetch(
-          "http://localhost:8080/api/v1/post",
+          "https://karnavision-dalle-clone.onrender.com/api/v1/post",
           {
             method: "POST",
             headers: {
